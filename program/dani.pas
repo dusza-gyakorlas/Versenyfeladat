00d0f165e2ddffdedbea51f553cb3231af4d7801
dantpu <47472856+dantpu@users.noreply.github.com>
2019-02-20 11:51:31 +0100

    Add files via upload

---
+++ program/dani.pas
@@ -0,0 +1,60 @@
+unit Dani;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+procedure lekerdezes(t:real);
+var x0,x1,y0,y1,v,v0,v1,t,t0,t1,a,s:real;
+  i,j:int32;
+
+begin
+
+  i:=1;
+  while (adatok[i].allapot<>0) or (adatok[i].t<t) do i:=i+1; //kiszámolja h melyik két mintavételi pont között van
+
+  j:=1;
+  while adatok[i-j].allapot=0 do j:=j+1;                  //motor állapota
+  if adatok[i-j].allapot=1 then writeln('Beindítva')
+  else begin
+   writeln('Leállítva');
+   end;
+
+
+   adatok[i-1].t=t then a:=adatok[i-1].a         //kiszámolja a gyorsulását
+   else a:=(adatok[i-1].a+adatok[i].a)/2;
+
+   x0:=adatok[i-1].x;                        //v0..megadja
+   x1:=adatok[i].x;
+   y0:=adatok[i-1].y;
+   y1:=adatok[i].y;
+   v0:=adatok[i-1].v;
+   v1:=adatok[i].v;
+   t0:=adatok[i-1].t;
+   t1:=adatok[i].t;
+    s:=v0*(t-t0)+(a/2)*sqr(t-t0);            //mekkora utat tett meg az autó a legutóbbi mintavétel óta
+
+    if x0<>x1 then begin
+      if x1>x0 then writeln(x0+s)
+      else writeln(x0-s);
+    end;                                    //autó pozícióját kiírja
+
+    if y0<>y1 then begin
+      if y1>y0 then writeln(y0+s)
+      else writeln(y0-s);
+    end;
+
+   v:=v0+a*(t-t0);                          //aktuális sebesség kiírása
+
+   writeln(a);                             //aktuális gyorsulás kiírása
+
+   if v*3,6>terkep[x,y] then writeln('Az autó túl gyorsan megy');
+
+   if a>2,5 then writeln('AZ autó hirtelen gyorsul');
+   if a<-2,5 then writeln('Az autó hirtelen lassul');
+end.
+
