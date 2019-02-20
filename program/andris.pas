72f9f300092e0ed78b4eec7d69712684608b187b
kovacsand <47793048+kovacsand@users.noreply.github.com>
2019-02-20 11:18:00 +0100

    Add files via upload
    Most már talán a teljes út is jó.

--- program/andris.pas
+++ program/andris.pas
@@ -17,8 +17,24 @@ end;
 
 function erkezik():string;
 begin
-  erkezik:=(adatok[adatokhossz].x);
-  erkezik:=erkezik+' '+adatok[adatokhossz].y;
+  erkezik:=(adatok[adatokhossz-1].x);
+  erkezik:=erkezik+' '+adatok[adatokhossz-1].y;
+end;
+
+function teljesut():integer;
+var i,j:integer;
+begin
+  teljesut:=0;
+  for i:=3 to hossz do begin
+    if adatok[i].allapot=0 then begin
+      for j:=i-1 downto 2 do begin
+        if adatok[j].allapot=0 then begin
+          teljesut:=teljesut + (sqrt (sqr((adatok[i].x)-(adatok[j].x)) +
+            sqr((adatok[i].y)-(adatok[j].y)) ));
+        end;
+      end;
+    end;
+  end;
 end;
 
 end.
